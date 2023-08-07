650c9e84e7559ac7d8ad6cb9ebf4c70e870981bf
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-07 16:27:42 -0500

    add task 33 minutes

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 
 from dotenv import load_dotenv
-from interactions import Intents
+from interactions import Intents, Task, IntervalTrigger, listen
 from interactions.ext.debug_extension import DebugExtension
 
 from core.init_logging import init_logging
 from core.base import CustomClient
 from core.extensions_loader import load_extensions
 
+from tasks import task
 
 if __name__ == "__main__":
     # load the environmental vars from the .env file
@@ -22,6 +23,15 @@ if __name__ == "__main__":
         auto_defer=True,  # automatically deferring interactions
         activity="Another interactions.py bot",  # the status message of the bot
     )
+
+    @Task.create(IntervalTrigger(minutes=33))
+    async def print_every_thirty_three():
+        print("It's been 33 minutes!")
+
+    # define a function to start the task on startup
+    @listen()
+    async def on_startup():
+        print_every_thirty_three.start()
 
     # load the debug extension if that is wanted
     if os.getenv("LOAD_DEBUG_COMMANDS") == "true":
