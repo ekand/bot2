fcc8b1149bf297f9a1e9411b8a1992a136aed303
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-07 20:03:41 -0500

    add feature hello_cam (for real this time I hope)

---
+++ extensions/hello_cam.py
@@ -0,0 +1,21 @@
+from core.base import CustomClient
+
+import os
+
+from interactions import Extension, InteractionContext, slash_command
+
+from dotenv import load_dotenv
+
+load_dotenv
+test_guild_id = os.getenv("TEST_GUILD_ID")
+
+class HelloCam(Extension):
+    bot: CustomClient
+
+    @slash_command(name="hello-cam", description="say hello to cam.", scopes=[test_guild_id])
+    async def hello_cam(self, ctx: InteractionContext):
+        await ctx.send("Hello from Cam")
+
+def setup(bot: CustomClient):
+    HelloCam(bot)
+
