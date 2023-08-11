de2b6c812debe58a7807a352788d858dc0056806
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-08-11 07:03:31 -0500

    put error handling back into make-error command

--- extensions/error.py
+++ extensions/error.py
@@ -16,8 +16,11 @@ class Error(Extension):
     @slash_command(name="make-error", description="Ping...")  # , scopes=[test_guild_id]
     async def ping(self, ctx: InteractionContext):
         await ctx.send("Trying the impossible...")
-        x = 1 / 0
-        await ctx.send("I did it!")
+        try:
+            x = 1 / 0
+            await ctx.send("I did it!")
+        except ZeroDivisionError:
+            await ctx.send("Something went wrong.")
 
 
 def setup(bot: CustomClient):
