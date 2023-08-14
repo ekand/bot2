1045078c4c02ce2597962ff65de5ca5cb78877f6
Erik K Anderson <erik@ekand.io>
2023-08-14 06:38:02 -0500

    add print statments for relevant information

--- src/prototypes/code_memory_prototype.py
+++ src/prototypes/code_memory_prototype.py
@@ -2,15 +2,18 @@ import json
 import os
 import random
 
+from dotenv import load_dotenv
 from github import Auth
 from github import Github
 
-# todo: save a json document indicating where the line of code was found
+load_dotenv()
+
+# todo: replace the print statements and save the information to a json file
 
 
 def get_random_line():
     """Calls the github API to retrieve repositories and eventually returns a single line of code."""
-    
+
     # Look up the GitHub auth token from the environment, then set up authentication
     auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
 
@@ -30,12 +33,12 @@ def get_random_line():
     while not content_files:
 
         # remove one repo from the list of repos and save it to a repo variable
-        repo = repos.pop()
+        chosen_repo = repos.pop()
 
         # call the Github api to get the contents of the repository, starting at the root ("")
-        
-        contents = repo.get_contents("")
-        
+
+        contents = chosen_repo.get_contents("")
+
         # Start a loop that will end when all of the content of the repo has been processed
         while len(contents) > 0:
             # remove one file_content object from the list of contents and store it in the file_content variable
@@ -44,7 +47,7 @@ def get_random_line():
             # if the type of the object found is a directory, get the contents from within that directory
             if file_content.type == "dir":
                 # add those content items to the list of content_files
-                contents.extend(repo.get_contents(file_content.path))
+                contents.extend(chosen_repo.get_contents(file_content.path))
             else:  # if the type is not a directory, then it is a file. Add it to the content_files list.
                 content_files.append(file_content)
 
@@ -55,15 +58,23 @@ def get_random_line():
             if content_file.path.endswith(".py")
         ]
 
+    print("chosen_repo_name:", chosen_repo.name)  # todo save me to json
+
     # Randomly choose one of the content files from the available list
-    content_file = random.choice(content_files)
+    chosen_file = random.choice(content_files)
+
+    chosen_file_path = chosen_file.path
+    print("chosen_file_path:", chosen_file_path)  # todo save me to json
+
     # Decode the content to access the text, then split it on the newline character
-    lines = content_file.decoded_content.decode("ASCII").split("\n")
-    
+    lines = chosen_file.decoded_content.decode("ASCII").split("\n")
+
     # start a loop that will continue until a non-empty line is found
     chosen_line = ""
     while not chosen_line.strip():
         chosen_line = random.choice(lines)
-    
+
+    print("chosen_line:", chosen_line)  # todo: save me to json
+
     # return that chosen line
     return chosen_line
