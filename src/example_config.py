71c6fd4a5b61efab0b53c621dc107a6fae554816
Erik Kristofer Anderson <52926724+ekand@users.noreply.github.com>
2023-08-15 07:07:25 -0500

    Update example_config.py
    remove confusing comments

--- src/example_config.py
+++ src/example_config.py
@@ -2,10 +2,9 @@
 DEV_MODE = True  # set False for production
 LOAD_DEBUG_COMMANDS = True  # set False for production.
 # Alternately, if DEV_MODE is False, no need to specify the rest of the dev section
-DEV_GUILD_ID = 123456798  # guild name
-DEV_CHANNEL_ID = 987654321  # channel name
-DEV_USER_ID = 123456789  # username
-
+DEV_GUILD_ID = 123456798
+DEV_CHANNEL_ID = 987654321
+DEV_USER_ID = 123456789
 # [general]
 USE_SENTRY = False  # set True for production
 DELETE_UNUSED_APPLICATION_CMDS = False  # Set True temporarily, as needed
